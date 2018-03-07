6558003c52e2b2ff38dd960141645165c87c7411
Juhi Gautam <JuhiGautam@wipro-h.local>
2018-03-07 16:19:42 +0530

    Initial Commit

---
+++ ItemInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ItemInfo
+//
+//  Created by Juhi Gautam on 07/03/18.
+//  Copyright © 2018 Juhi Gautam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ItemInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ItemInfo
+//
+//  Created by Juhi Gautam on 07/03/18.
+//  Copyright © 2018 Juhi Gautam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
